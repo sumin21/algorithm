f4527ed991e4735437cbe12d8f6a398209d386de
sumin21 <tnals1178@naver.com>
2023-03-17 17:38:43 +0900

    백준 - 14002

---
+++ src/Baekjoon14002.java
@@ -0,0 +1,59 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon14002 {
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int n;
+    public static int[] arr;
+    public static Integer[] dp;
+    public static int max=0;
+
+    public void Baekjoon14002() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        dp = new Integer[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i=0; i<n; i++) {
+            int res = lis(i);
+            if (max < res) max = res;
+        }
+        bw.write(String.valueOf(max));
+        bw.newLine();
+        int value = max;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n-1; i >= 0; i--) {
+            if (value == dp[i]) {
+                stack.push(arr[i]);
+                value--;
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            bw.write(String.valueOf(stack.pop()) + " ");
+        }
+
+        bw.close();
+    }
+
+    public static int lis(int a) {
+        if (dp[a] == null) {
+            dp[a] = 1;
+
+            for (int i=a-1; i>=0; i--) {
+                if (arr[i] < arr[a]) {
+                    dp[a] = Math.max(dp[a], dp[i]+1);
+                }
+            }
+        }
+
+        return dp[a];
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] ars) throws IOException {
-        Baekjoon11722 algorithm = new Baekjoon11722();
-        algorithm.Baekjoon11722();
+        Baekjoon11055 algorithm = new Baekjoon11055();
+        algorithm.Baekjoon11055();
     }
 }
